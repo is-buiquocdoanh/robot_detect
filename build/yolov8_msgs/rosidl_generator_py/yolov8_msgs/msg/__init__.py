6d08a5881d07a77c80d937193827da9b42d3af3f
Jayson_Bui <doanh762003@gmail.com>
2025-10-15 13:01:30 +0700

    fix .gitignore

--- build/yolov8_msgs/rosidl_generator_py/yolov8_msgs/msg/__init__.py
+++
@@ -1,4 +0,0 @@
-from yolov8_msgs.msg._human_position import HumanPosition  # noqa: F401
-from yolov8_msgs.msg._human_position_array import HumanPositionArray  # noqa: F401
-from yolov8_msgs.msg._inference_result import InferenceResult  # noqa: F401
-from yolov8_msgs.msg._yolov8_inference import Yolov8Inference  # noqa: F401
